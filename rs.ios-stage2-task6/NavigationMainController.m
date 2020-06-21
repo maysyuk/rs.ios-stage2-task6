f55ef328e63e3bedb94fea3bec397da235d815c8
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2020-06-22 01:43:51 +0300

    open tab bar controller when user taps start button

--- rs.ios-stage2-task6/NavigationMainController.m
+++ rs.ios-stage2-task6/NavigationMainController.m
@@ -4,6 +4,7 @@
 //
 
 #import "NavigationMainController.h"
+#import "TabBarController.h"
 
 
 @implementation NavigationMainController
@@ -12,6 +13,26 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor whiteColor];
+
+    [self startButton];
 }
+
+- (void)startButton {
+    UIButton *startButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    startButton.frame = CGRectMake(50.0, 500.0, 300.0, 50.0);
+    startButton.backgroundColor = [UIColor yellowColor];
+    [startButton setTitle:@"START" forState:UIControlStateNormal];
+    [startButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    startButton.titleLabel.font = [UIFont systemFontOfSize:20 weight:UIFontWeightMedium];
+    //startButton.clipsToBounds = YES;
+    startButton.layer.cornerRadius = 55.0 / 2.0f;
+    [startButton addTarget:self action:@selector(startTapped) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startButton];
+}
+
+- (void)startTapped {
+    [self.navigationController pushViewController: [TabBarController new] animated:NO];
+}
+
 
 @end
---
+++ rs.ios-stage2-task6/TabBarController.m
@@ -0,0 +1,21 @@
+//
+// Created by Eugene Maysyuk on 6/22/20.
+// Copyright (c) 2020 Eugene Maysyuk. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+
+@implementation TabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self setBackground];
+}
+
+- (void)setBackground {
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
